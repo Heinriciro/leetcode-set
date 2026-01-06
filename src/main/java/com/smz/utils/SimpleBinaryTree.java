6dc93e62d446746df2c50fe56d75d32d28f82bd7
Heinriciro <ianhuang15@outlook.com>
2026-01-06 16:03:47 +0800

    Add a generic universal binary tree class for convenience of use.
    Signed-off-by: Heinriciro <ianhuang15@outlook.com>

---
+++ src/main/java/com/smz/utils/SimpleBinaryTree.java
@@ -0,0 +1,49 @@
+package com.smz.utils;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+public class SimpleBinaryTree<T1> {
+    public SimpleBinaryTreeNode<T1> root;
+
+    public SimpleBinaryTree(T1[] values, Class<? extends SimpleBinaryTreeNode<T1>> clazz, Class<T1> typeVariableClazz)
+        throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        if (values == null || values.length == 0) {
+            root = null;
+            return;
+        }
+
+        Constructor<? extends SimpleBinaryTreeNode<T1>> nodeConstructor = clazz.getConstructor(typeVariableClazz);
+        
+        root = buildTree(values, 0, nodeConstructor);
+    }
+
+    private SimpleBinaryTreeNode<T1> buildTree(T1[] values, int index, Constructor<? extends SimpleBinaryTreeNode<T1>> nodeConstructor)
+        throws NoSuchMethodException, InstantiationException, IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        if (index >= values.length || values[index] == null) {
+            return null;
+        }
+
+        SimpleBinaryTreeNode<T1> node = nodeConstructor.newInstance(values[index]);
+        node.left = buildTree(values, 2 * index + 1, nodeConstructor);
+        node.right = buildTree(values, 2 * index + 2, nodeConstructor);
+        return node;
+    }
+
+    public static class SimpleBinaryTreeNode<T2> {
+        public T2 val;
+        public SimpleBinaryTreeNode<T2> left;
+        public SimpleBinaryTreeNode<T2> right;
+
+        public SimpleBinaryTreeNode(T2 x) {
+            val = x;
+        }
+        public SimpleBinaryTreeNode(T2 x, SimpleBinaryTreeNode<T2> left, SimpleBinaryTreeNode<T2> right) {
+            val = x;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
+
